99014cdecec59bb7297c93a1c5ec6beb6ac0b704
luckygirlalisa <zfxiao@thoughtworks.com>
2014-05-20 11:29:48 +0800

    use custom servlet to display add page.

---
+++ src/main/java/main/java/MyServlet.java
@@ -0,0 +1,17 @@
+package main.java;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/Book-shelf-home.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
